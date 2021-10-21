7990521fe7629a96d3949ae12e157ca1668834e7
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-10-21 10:42:21 +0900

    shimmitsu-10-21

---
+++ venv_Sotuken/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\h_shimmitsu\documents\sotuken\venv_sotuken\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv_Sotuken/sotuken/tamesi1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Tamesi1Config(AppConfig):
+    name = 'tamesi1'
