79af3545b38b035cc615eed1776ee192ba372d6e
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-10-22 14:19:54 +0900

    10/22

--- venv_Sotuken/sotuken/tamesi1/urls.py
+++ venv_Sotuken/sotuken/tamesi1/urls.py
@@ -7,4 +7,5 @@ from . import views
 app_name = 'tamesi1'
 urlpatterns = [
   path('',TemplateView.as_view(template_name = 'index.html'), name='index'),
+  path('base/',views.BaseView.as_view(),name='base'),
 ]
