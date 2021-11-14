1d548f8d829f9526f7fe9b3089184e7215d0b1a9
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-11-15 00:02:26 +0900

    11/15

---
+++ apps/gpubenchmas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GpubenchmasConfig(AppConfig):
+    name = 'gpubenchmas'
---
+++ apps/itemspec/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ItemspecConfig(AppConfig):
+    name = 'itemspec'
