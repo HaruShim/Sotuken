3ed35cc71e59673eee19297d2baa4eeb9a8e4e4c
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-11-15 23:23:50 +0900

    11/15

--- venv_Sotuken/sotuken/apps/tamesi1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Tamesi1Config(AppConfig):
-    name = 'tamesi1'
--- venv_Sotuken/sotuken/apps/tamesi1/views.py
+++
@@ -1,6 +0,0 @@
-from django.views import generic
-
-# from .forms import BaseForm
-
-class BaseView(generic.TemplateView):
-  template_name = 'base.html'
