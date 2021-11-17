d6d5183f29402b2191a6814e10c59a6d8415a955
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-11-17 11:51:51 +0900

    11/17

--- 実装/apps/itemspec/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ItemspecConfig(AppConfig):
-    name = 'itemspec'
--- 実装/apps/itemspec/views.py
+++
@@ -1,5 +0,0 @@
-from django.views import generic
-
-class ItemSpecView(generic.TemplateView):
-    template_name = ""
-
