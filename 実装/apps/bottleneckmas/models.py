397b350a37c001f062f38633e372dab51acc51d4
wutianci <tky2002083@stu.o-hara.ac.jp>
2021-11-17 10:45:18 +0900

    11/17

--- 実装/apps/bottleneckmas/models.py
+++ 実装/apps/bottleneckmas/models.py
@@ -1,3 +1,20 @@
+from re import VERBOSE
 from django.db import models
 
-# Create your models here.
+
+class Bottleneck(models.Model):
+    bottleneck_id = models.IntegerField(verbose_name='ボトルネックID')
+    cpu_name = models.CharField(verbose_name='CPU名', max_length=30)
+    gpu_name = models.CharField(verbose_name='GPU名', max_length=30)
+    balance = models.SmallIntegerField(verbose_name='バランス')
+    wqhd = models.SmallIntegerField(verbose_name='WQHD')
+    four_k = models.SmallIntegerField(verbose_name='4K解像度')
+    game_appropriate = models.CharField(verbose_name='ゲーム適正', max_length=2)
+    game_delivery = models.SmallIntegerField(verbose_name='ゲーム配信')
+    video_editing = models.SmallIntegerField(verbose_name='動画編集')
+    encode = models.SmallIntegerField(verbose_name='エンコード')
+    created_at = models.DateTimeField(verbose_name='作成日時')
+    updated_at = models.DateTimeField(verbose_name='最終更新日時', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'ボトルネックモデル'
