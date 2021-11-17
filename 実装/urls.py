40b1db302beb5ae61cabe5dc8ee7697371eb9c95
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-11-17 14:57:40 +0900

    11/17

--- 実装/urls.py
+++ 実装/urls.py
@@ -5,33 +5,21 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    # 従業員マスタ
-    path('F01/',include('employeemas.urls')),
-    # 店舗マスタ
-    path('F02/',include('storemas.urls')),
-    # 商品マスタ
-    path('F03/',include('itemmas.urls')),
-    # 売上マスタ
-    path('F04/',include('earningmas.urls')),
-    # ベンチマークマスタ
-    path('F05/',include('benchmas.urls')),
-    # ボトルネックマスタ
-    path('F06/',include('bottleneckmas.urls')),
-    # 在庫
-    path('F07/',include('stock.urls')),
-    # 検索
-    path('F08/',include('search.urls')),
-    # 取置予約
-    path('F09/',include('reserve.urls')),
-    # 見積
-    path('F10/',include('estimate.urls')),
-    # 売上
-    path('F11/',include('earning.urls')),
-    # 従業員参照
-    path('F12/',include('employeeref.urls')),
-    # 店舗参照
-    path('F13/',include('storeref.urls')),
-    # 勤務状況編集
-    path('F14/',include('workstatus.urls')),
+    # path('tamesi/',include('bottletamesi.urls')),
+    
+    path('F01/',include('employeemas.urls')),# 従業員マスタ
+    path('F02/',include('storemas.urls')),# 店舗マスタ
+    path('F03/',include('itemmas.urls')),# 商品マスタ
+    path('F04/',include('earningmas.urls')),# 売上マスタ
+    path('F05/',include('benchmas.urls')),# ベンチマークマスタ
+    path('F06/',include('bottleneckmas.urls')),# ボトルネックマスタ
+    path('F07/',include('stock.urls')),# 在庫
+    path('F08/',include('search.urls')),# 検索
+    path('F09/',include('reserve.urls')),# 取置予約
+    path('F10/',include('estimate.urls')),# 見積
+    path('F11/',include('earning.urls')),# 売上
+    path('F12/',include('employeeref.urls')),# 従業員参照
+    path('F13/',include('storeref.urls')),# 店舗参照
+    path('F14/',include('workstatus.urls')),# 勤務状況編集
 
 ]
