7b799b11b9b96159bfaab4ddb63259e6d774079e
tky2002110 <tky2002110@stu.o-hara.ac.jp>
2021-11-18 11:33:42 +0900

    11/18

--- 一時保存用/benchmas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-appname = 'bench'
-urlpatterns = [
-    path('',views.BenchView.as_view(),name = "master_bench"),
-]
--- 一時保存用/benchmas/views.py
+++
@@ -1,6 +0,0 @@
-from django.views import generic
-
-
-class BenchView(generic.TemplateView):
-    template_name = "sample_master5.html"
-
