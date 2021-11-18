d7f55c318f1c4133fd965c1b273ae268d4f7eb31
wutianci <tky2002083@stu.o-hara.ac.jp>
2021-11-18 11:45:29 +0900

    11/18

--- 実装/呉models/benchmas/models.py
+++ 実装/呉models/benchmas/models.py
@@ -21,8 +21,8 @@ class GpuBench(models.Model):
     """
     id = models.AutoField(verbose_name='GPUベンチマークID',db_column='gpu_bench_id', primary_key=True, editable=False)
     gpu_name = models.CharField(verbose_name='GPU名称', max_length=30)
-    marktimespygraphics = models.IntegerField(verbose_name='Mark Time Spy Graphics', max_length=7)
-    tdp = models.IntegerField(verbose_name='TDP', max_length=5)
+    marktimespygraphics = models.IntegerField(verbose_name='Mark Time Spy Graphics')
+    tdp = models.IntegerField(verbose_name='TDP')
     cost_performance = models.FloatField(verbose_name='コスパ', max_length=4)
     wattage_performance = models.FloatField(verbose_name='ワットパフォーマンス', max_length=6)
     list_price = models.IntegerField(verbose_name='定価')
--- 実装/呉models/earningmas/models.py
+++ 実装/呉models/earningmas/models.py
@@ -25,9 +25,9 @@ class EarningInfo(models.Model):
     item_code = models.CharField(verbose_name='商品コード', max_length=9)
     store_id = models.SmallIntegerField(verbose_name='店舗ID', max_length=3)
     employee_id = models.CharField(verbose_name='従業員ID', max_length=13)
-    selling_price = models.IntegerField(verbose_name='販売価格', max_length=8)
-    gross_profit = models.IntegerField(verbose_name='粗利', max_length=8)
-    gross_profit_margin = models.IntegerField(verbose_name='粗利率', max_length=4)
+    selling_price = models.IntegerField(verbose_name='販売価格')
+    gross_profit = models.IntegerField(verbose_name='粗利')
+    gross_profit_margin = models.IntegerField(verbose_name='粗利率')
     sale_date = models.DateTimeField(verbose_name='販売日', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='最終更新日時', auto_now=True)
 
