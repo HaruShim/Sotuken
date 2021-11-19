ad6ee2513305eaa99776a84081ea5bbd063895ce
wutianci <tky2002083@stu.o-hara.ac.jp>
2021-11-19 09:46:36 +0900

    11/19

--- 実装/apps/earningmas/models.py
+++ 実装/apps/earningmas/models.py
@@ -20,14 +20,13 @@ class EarningInfo(models.Model):
     売上情報モデル
 
     """
-
-    earning_id = models.IntegerField(verbose_name='売上情報ID')
+    id = models.AutoField(verbose_name='売上情報ID', db_column='earning_id', primary_key=True, editable=False)
     item_code = models.CharField(verbose_name='商品コード', max_length=9)
     store_id = models.SmallIntegerField(verbose_name='店舗ID')
     employee_id = models.CharField(verbose_name='従業員ID', max_length=13)
     selling_price = models.IntegerField(verbose_name='販売価格')
     gross_profit = models.IntegerField(verbose_name='粗利')
-    gross_profit_margin = models.IntegerField(verbose_name='粗利率', max_length=4)
+    gross_profit_margin = models.IntegerField(verbose_name='粗利率')
     sale_date = models.DateTimeField(verbose_name='販売日', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='最終更新日時', auto_now=True)
 
--- 実装/apps/itemmas/models.py
+++ 実装/apps/itemmas/models.py
@@ -3,8 +3,8 @@
 * DBの商品情報モデルにアクセスするためのモジュール
 
 Todo:
-
     新満の確認
+
     *マイグレーションができるか確認
     *CSVファイルからデータをコピーできる
     *新満の最終確認
@@ -14,19 +14,19 @@ from django.db import models
 
 
 class ItemInfo(models.Model):
-    
+
     """ ItemInfo
 
     商品情報モデル
 
     """
-    item_code = models.CharField(verbose_name='商品コード', max_length=9)
-    store_id = models.IntegerField(verbose_name='店舗ID', max_length=3)
+    id = models.AutoField(verbose_name='商品コード', db_column='item_code', primary_key=True, editable=False)
+    store_id = models.IntegerField(verbose_name='店舗ID')
     model_number = models.CharField(verbose_name='型番', max_length=30)
-    category = models.SmallIntegerField(verbose_name='カテゴリ', max_length=1)
+    category = models.SmallIntegerField(verbose_name='カテゴリ')
     manufacturer_name = models.CharField(verbose_name='メーカー名', max_length=30)
     item_status = models.CharField(verbose_name='商品状態', max_length=10)
-    purchase_price = models.IntegerField(verbose_name='仕入価格', max_length=20)
+    purchase_price = models.IntegerField(verbose_name='仕入価格')
     item_image = models.ImageField(verbose_name='商品画像')
     remarks = models.TimeField(verbose_name='備考')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
