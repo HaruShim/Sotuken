f10b1576ff65e6aed0f155c32066c05e590af810
wutianci <tky2002083@stu.o-hara.ac.jp>
2021-11-19 09:52:17 +0900

    11/19

--- 実装/apps/benchmas/models.py
+++ 実装/apps/benchmas/models.py
@@ -3,7 +3,11 @@
 *
 Todo:
 
-    *確認待ち
+新満の確認
+
+    *マイグレーションができるか確認
+    *CSVファイルからデータをコピーできる
+    *新満の最終確認
 
 """
 from django.db import models
@@ -16,13 +20,12 @@ class GpuBench(models.Model):
     Gpuベンチマーク
 
     """
-
-    gpu_bench_id = models.IntegerField(verbose_name='GPUベンチマークID', max_length=4)
+    id = models.AutoField(verbose_name='GPUベンチマークID', db_column='gpu_bench_id', primary_key=True, editable=False)
     gpu_name = models.CharField(verbose_name='GPU名称', max_length=30)
-    marktimespygraphics = models.IntegerField(verbose_name='Mark Time Spy Graphics', max_length=7)
-    tdp = models.IntegerField(verbose_name='TDP', max_length=5)
-    cost_performance = models.FloatField(verbose_name='コスパ', max_length=4)
-    wattage_performance = models.FloatField(verbose_name='ワットパフォーマンス', max_length=6)
+    marktimespygraphics = models.IntegerField(verbose_name='Mark Time Spy Graphics')
+    tdp = models.IntegerField(verbose_name='TDP')
+    cost_performance = models.FloatField(verbose_name='コスパ')
+    wattage_performance = models.FloatField(verbose_name='ワットパフォーマンス')
     list_price = models.IntegerField(verbose_name='定価')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
@@ -37,7 +40,7 @@ class CpuBench(models.Model):
     Cpuベンチマーク
 
     """
-    cpu_bench_id = models.IntegerField(verbose_name='CPUベンチマークID', max_length=4)
+    id = models.AutoField(verbose_name='CPUベンチマークID',db_column='cpu_bench_id', primary_key=True, editable=False)
     cpu_name = models.CharField(verbose_name='CPU名称', max_length=30)
     passmark = models.IntegerField(verbose_name='PassMark', max_length=7)
     core = models.IntegerField(verbose_name='コア', max_length=4)
--- 実装/apps/employeemas/models.py
+++ 実装/apps/employeemas/models.py
@@ -2,7 +2,12 @@
 
 Todo:
 
-    *確認待ち
+新満の確認
+
+    *マイグレーションができるか確認
+    *CSVファイルからデータをコピーできる
+    *新満の最終確認
+
 """
 from django.db import models
 
@@ -10,15 +15,15 @@ from django.db import models
 class Employeemas(models.Model):
     """従業員情報モデル
     """
-    employee_id = models.CharField(verbose_name='従業員ID', max_length=13)
-    store_id = models.SmallIntegerField(verbose_name='店舗ID', max_length=3)
+    id = models.AutoField(verbose_name='従業員ID', db_column='employee_id', primary_key=True, editable=False)
+    store_id = models.SmallIntegerField(verbose_name='店舗ID')
     employment_status = models.CharField(verbose_name='雇用形態', max_length=5)
     name = models.CharField(verbose_name='氏名', max_length=30)
     ruby_name = models.CharField(verbose_name='氏名(カナ)', max_length=30)
     tel_num = models.CharField(verbose_name='電話番号', max_length=13)
-    gender = models.SmallIntegerField(verbose_name='従業員ID', max_length=3)
+    gender = models.SmallIntegerField(verbose_name='従業員ID')
     birthday = models.TimeField(verbose_name='生年月日')
-    invalid_flg = models.SmallIntegerField(verbose_name='無効フラグ', max_length=1)
+    invalid_flg = models.SmallIntegerField(verbose_name='無効フラグ')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     # 最終ログイン日時修正予定
     lastlogin_at = models.DateTimeField(verbose_name='最終ログイン日時', auto_now_add=True)
