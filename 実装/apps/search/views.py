8f79dce03e9cf3709cebb810916b26432038fc5a
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-12-01 10:57:17 +0900

    12/1

--- 実装/apps/search/views.py
+++ 実装/apps/search/views.py
@@ -1,12 +1,21 @@
-from django.views import generic
+from django.views.generic import TemplateView,ListView
+from benchmas.models import CpuBench,GpuBench
+from bottleneckmas.models import Bottleneck
+from itemmas.models import ItemInfo
 
-class S0801View(generic.TemplateView):
-    template_name = "benhmark_search.html"
-
-class S0802View(generic.TemplateView):
-    template_name = "bottleneck_search.html"
-
-class S0803View(generic.TemplateView):
+class S0801View(ListView):
     template_name = "item_search.html"
+    model = ItemInfo
+    paginate_by = 12
+
+class S0802View(ListView):
+    template_name = "bottleneck_search.html"
+    model = Bottleneck
+    paginate_by = 12
+
+class S0803View(ListView):
+    template_name = "benchmark_search.html"
+    model = CpuBench
+    paginate_by = 12
 
 
