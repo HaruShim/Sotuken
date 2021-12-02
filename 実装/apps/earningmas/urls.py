d3b840160d84059548e464f579bb88ca587d9c38
HaruShim <tky2002100@stu.o-hara.ac.jp>
2021-12-03 03:12:58 +0900

    12/3

--- 実装/apps/earningmas/urls.py
+++ 実装/apps/earningmas/urls.py
@@ -13,8 +13,8 @@ from . import views
 
 app_name = 'earningmas'
 urlpatterns = [
-    path('S04-01/',views.S0401View.as_view(),name = "S04-01"), #売上情報一覧
-    path('S04-02/',views.S0402View.as_view(),name = "S04-02"), #売上情報詳細
-    path('S04-03/',views.S0403View.as_view(),name = "S04-03"), #売上情報編集（返品・返金）
+    path('S04-01/',views.S0401View.as_view(),name = "S04-01"),
+    path('S04-02/',views.S0402View.as_view(),name = "S04-02"),
+    path('S04-03/',views.S0403View.as_view(),name = "S04-03"),
 ]
 
