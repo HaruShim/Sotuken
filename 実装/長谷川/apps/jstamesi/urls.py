48498f2653e2ab54dc1582387dae90b9529ee185
hasegawak333 <tky2002134@stu.o-hara.ac.jp>
2022-02-16 18:28:36 +0900

    0216

--- 実装/長谷川/apps/jstamesi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'jstamesi'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('ajax-number/', views.ajax_number, name='ajax_number'),
-]
